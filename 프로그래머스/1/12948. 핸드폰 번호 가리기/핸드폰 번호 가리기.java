dce01fa80d2e5129bf88655a57d9a35a5cc7b6c4
StrayCat <160587239+straycat405@users.noreply.github.com>
2026-02-20 10:29:35 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.05 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String phone_number) {
+        
+        char[] arr = phone_number.toCharArray();
+        
+        for (int i = 0 ; i < arr.length - 4 ; i ++) {
+            arr[i] = '*';
+        }
+        
+        return new String(arr);
+        
+        
+        
+
+        
+    }
+}
