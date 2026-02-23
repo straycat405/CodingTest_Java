8511c0e1fee0c981965847d1b97e6eb905897db9
StrayCat <160587239+straycat405@users.noreply.github.com>
2026-02-23 10:25:04 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.16 ms, Memory: 83.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(int n) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0 ; i < n ; i ++) {
+            if (i % 2 == 1) {
+                sb.append("박");
+            } else {
+                sb.append("수");
+            }
+        }
+        return sb.toString();
+    }
+}
